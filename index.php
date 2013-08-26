2a00156521123722a7d4e763e820354b02ed482b
conanxin <conanxin@gmail.com>
2013-08-26 11:25:34 +0800

    add files

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <title>兰迪·波许教授的最后一课</title>
+        <style>
+            *{
+                margin:0;
+                padding:0;
+            }
+            .knr{
+                margin:0 auto;
+                padding:0 auto;
+                width:540px;
+            }
+            .tit{
+                background:url('./img/tit.png') no-repeat;
+                height:40px;
+                padding-top:20px;
+                padding-left:20px;
+                font:normal bold 12pt serif;
+                color:#026ea3;
+            }
+            .main{
+                float:left;
+                border-left:1px solid #a9c9d0;
+                border-right:1px solid #a9c9d0;
+                border-bottom:1px solid #a9c9d0;
+                width:538px;
+            }
+            .inp{
+                float:left;
+                width:300px;
+                margin-left:20px;
+                margin-top:1px;
+				margin-bottom:1px;
+            }
+            .sximg{
+                float:left;
+                border:1px solid #a9c9d0;
+                width:170;
+                height:170;
+    
+               margin-bottom:10px; 
+            }
+        </style>
+    </head>
+    <body>
+        <div class="knr">
+            <div class="tit">
+                兰迪·波许教授的最后一课
+            </div>
+            <div class="main">
+                <div class="inp">
+                    <form action="" method="post">
+                        <embed src="randy.swf" width="500" height="400"></embed>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
+    <script src="http://app.baidu.com/static/appstore/monitor.st"></script>
+    <script>
+        baidu.app.autoHeight();
+        baidu.app.setHeight(200);
+    </script>
+    
+</html>
